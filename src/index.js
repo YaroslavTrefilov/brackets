9ea2107d070f3e968b0432df46835b2369bce963
YaroslavTrefilov <yaroslavtrefilov5@gmail.com>
2020-10-06 17:04:56 +0400

    complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+  let lastStr = str;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      arr.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  }
+  while (true) {
+      for (let i = 0; i < arr.length; i++) {
+          str = str.replace(arr[i], '');
+      }
+      if (str.length == 0) {
+          return true;
+      }
+      if (lastStr === str) {
+          return false;
+      }
+      lastStr = str;
+  }
+};
